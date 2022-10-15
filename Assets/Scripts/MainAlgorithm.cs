c2728ab30edfb46f29d9ca51c5fe59cfafd2df5b
Peter Vahlberg <pvahlberg@zagmail.gonzaga.edu>
2022-10-15 13:16:45 -0700

    fixed bug and completed values

--- Assets/Scripts/MainAlgorithm.cs
+++ Assets/Scripts/MainAlgorithm.cs
@@ -5,29 +5,43 @@ using UnityEngine;
 public class MainAlgorithm : MonoBehaviour
 {
     private int length = 4;
-    private int[] pattern = {0, 1, 2, 3, 1, 2, 1, 2, 3, 1, 5, 4, 6, 2, 3, 4, 1, 3, 2};
+
+    private int[] testArray = {0, 1, 2, 3, 1, 2, 1, 2, 3, 1, 5, 4, 6, 2, 3, 4, 1, 3, 2};
+    private List<int> dataArray;
+
     private List<Pattern> patterns;
 
     // Start is called before the first frame update
     void Start()
     {
+        dataArray = new List<int>(testArray);
+        length = dataArray.Count;
+
         GetPrediction();
     }
 
     public void GetPrediction()
     {
         patterns = new List<Pattern>();
-        //GetPatternList();
+        
+        GetPatternList();
 
+        /*
         Pattern p = new Pattern();
         p.addToPattern(1);
         p.addToPattern(2);
         patterns.Add(p);
+        */
 
-
-        DisplayPatterns();
+        
 
         CheckPatterns();
+
+        DisplayPatternsSelective();
+
+        int index = GetMostValueablePattern();
+
+        Debug.Log("Most valuable: " + patterns[index].ToString());
 
 
     }
@@ -47,7 +61,7 @@ public class MainAlgorithm : MonoBehaviour
                 for (int i = 0; i < patternSize; i++)
                 {
                     int positionInArray = i + arrayIndex;
-                    p.addToPattern(pattern[positionInArray]);
+                    p.addToPattern(dataArray[positionInArray]);
                     
 
                 }
@@ -66,34 +80,99 @@ public class MainAlgorithm : MonoBehaviour
     {
         for (int i = 0; i < patterns.Count; i++)
         {
-            Debug.Log("Pattern: " + patterns[i].ToString());
+            Debug.Log("Pattern: " + patterns[i].ToString() + " Value: " + patterns[i].GetVal());
         }
     }
 
+    private void DisplayPatternsSelective()
+    {
+        for (int i = 0; i < patterns.Count; i++)
+        {
+            if (patterns[i].GetVal() <= 1) { continue; }
+            Debug.Log("Pattern: " + patterns[i].ToString() + " Value: " + patterns[i].GetVal());
+        }
+    }
+
+    private int GetMostValueablePattern()
+    {
+        int valueableIndex = -1;
+        double bestVal = 0;
+        for (int i = 0; i < patterns.Count; i++)
+        {
+            double val = patterns[i].SortValue();
+            if (val > bestVal)
+            {
+                bestVal = val;
+                valueableIndex = i;
+            }
+
+        }
+        return valueableIndex;
+    }
+
+    private int FindNextValue()
+    {
+        int valueableIndex = -1;
+        double bestVal = 0;
+        for (int i = 0; i < patterns.Count; i++)
+        {
+            double correctness = patterns[i].PredictionCorrectness(dataArray);
+
+            if (correctness < 0.8)
+            {
+                correctness = 0;
+            }
+            double rawVal = patterns[i].GetVal();
+            double val = correctness * rawVal;
+
+            if (val > 0)
+            {
+                Debug.Log("FinalVal: " + val);
+            }
+
+            if (val > bestVal)
+            {
+                bestVal = val;
+                valueableIndex = i;
+            }
+
+        }
+        
+
+
+        return 0;
+    }
+
+
+
+
+
+
     private void CheckPatterns()
     {
+        
         //Getting all patterns
         for (int i = 0; i < patterns.Count; i++)
         {
 
             int arrayIndex = 0;
 
-            while (arrayIndex + patterns[i].Count() <= length)
+            while (arrayIndex + patterns[i].Count() < length)
             {
-                
-                
+                double val = patterns[i].Compare(dataArray, arrayIndex);
+                patterns[i].RemoveInsignificantValues();
+
+
 
                 arrayIndex++;
             }
-
-
-
         }
 
     }
 
 
 
+    
 
 
 
--- Assets/Scripts/Pattern.cs
+++ Assets/Scripts/Pattern.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Pattern : MonoBehaviour
 {
     private List<int> pattern;
-    
+
+    private double value;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +25,19 @@ public class Pattern : MonoBehaviour
     {
         return pattern.Count;
     }
+    public void SetVal(double val)
+    {
+        value = val;
+    }
+    public void AddVal(double val)
+    {
+        value += val;
+    }
 
+    public double GetVal()
+    {
+        return value;
+    }
     public override string ToString()
     {
         string s = "[";
@@ -40,8 +53,81 @@ public class Pattern : MonoBehaviour
 
     public double Compare(List<int> array, int startIndex)
     {
+        int length = pattern.Count;
+        int correct = 0;
 
-        return 1.0;
+        for (int i = startIndex; i < startIndex + length; i++)
+        {
+            if (array[i] == pattern[i - startIndex])
+            {
+                correct++;
+            }
+        }
+        value = (double)correct /(double)length;
+        return (double)correct/(double)length;
+    }
+
+    public void RemoveInsignificantValues()
+    {
+        if (pattern.Count > 8)
+        {
+            if (value < 0.9)
+            {
+                value = 0;
+            }
+        }
+        else if (pattern.Count > 5)
+        {
+            if (value < 0.8)
+            {
+                value = 0;
+            }
+        }
+        else if (pattern.Count > 3)
+        {
+            if (value < 0.7)
+            {
+                value = 0;
+            }
+        }
+        else if (pattern.Count > 1)
+        {
+            if (value < 0.9)
+            {
+                value = 0;
+            }
+        }
+
+    }
+
+
+
+    public double PredictionCorrectness(List<int> array)
+    {
+        int length = pattern.Count - 1;
+        int arrayLen = array.Count;
+        int correct = 0;
+        int patternIndex = 0;
+        for (int i = arrayLen - length - 1; i < arrayLen; i++)
+        {
+            if (array[i] == pattern[patternIndex])
+            {
+                correct++;
+            }
+            patternIndex++;
+        }
+
+
+        return (double)correct / (double)length;
+    }
+
+
+
+    public double SortValue()
+    {
+        double inverseVal = 1.0 - (1.0 / ((double)pattern.Count * 0.75));
+        double final = inverseVal * value;
+        return final;
     }
 
 
